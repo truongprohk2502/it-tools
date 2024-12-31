fb6031747b0493c0fb1711c4710eb0e66ea65350
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2024-12-31 11:51:55 +0700

    task: init app

---
+++ providers/theme-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from "react";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider> & {
+  children: React.ReactNode;
+}) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
